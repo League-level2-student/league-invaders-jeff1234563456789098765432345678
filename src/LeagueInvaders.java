a307e220621b8659c77d23fd0d2cd423aa75420a
Shivam <shivam.pansuria@gmail.com>
2020-09-24 18:30:31 -0700

    LeagueSnake,LeagueInvaders:IP:

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame holder;
+	final int WIDTH = 500;
+	final int HEIGHT = 800;
+	GamePanel gp;
+	public static void main(String[] args) {
+		LeagueInvaders league = new LeagueInvaders();
+league.setup();
+	}
 
+	public LeagueInvaders() {
+		holder = new JFrame();
+		gp = new GamePanel();
+	}
+
+	void setup() {
+		holder.add(gp);
+holder.setSize(WIDTH, HEIGHT);
+holder.setVisible(true);
+holder.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
 }
